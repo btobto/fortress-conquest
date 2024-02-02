1cce883b8ce2f36a299799d8aa70475dff186d0b
btobto <btosic16@gmail.com>
2024-02-02 12:01:17 +0100

    map radius circle

--- app/src/main/java/com/example/fortressconquest/ui/screens/map/components/MapScreenContent.kt
+++ app/src/main/java/com/example/fortressconquest/ui/screens/map/components/MapScreenContent.kt
@@ -3,6 +3,7 @@ package com.example.fortressconquest.ui.screens.map.components
 import android.location.Location
 import android.util.Log
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.fortressconquest.common.animateToLocation
@@ -29,6 +31,7 @@ import com.example.fortressconquest.ui.screens.map.MapViewModel
 import com.example.fortressconquest.ui.screens.map.TAG
 import com.google.android.gms.maps.LocationSource
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.Circle
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
@@ -84,6 +87,11 @@ fun MapScreenContent(
     val currentUserState by mapViewModel.currentUserState.collectAsStateWithLifecycle(AuthState.Loading)
     val filtersState by mapViewModel.filtersState.collectAsStateWithLifecycle()
     val fortressesState by mapViewModel.fortressesState.collectAsStateWithLifecycle()
+
+    val circleRadiusState by animateFloatAsState(
+        targetValue = filtersState.radiusInM.toFloat(),
+        label = "Circle radius animation"
+    )
 
     val locationSource = remember {
         object : LocationSource {
@@ -192,6 +200,19 @@ fun MapScreenContent(
                             onFortressClicked(fortress)
                             true
                         }
+                    )
+                }
+
+                val location = locationState
+                if (location is Response.Success) {
+                    Circle(
+                        center = LatLng(
+                            location.data.latitude,
+                            location.data.longitude
+                        ),
+                        radius = circleRadiusState.toDouble(),
+                        fillColor = Color.Blue.copy(alpha = 0.1f),
+                        strokeWidth = 0f
                     )
                 }
             }
